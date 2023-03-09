98163658721854929bc69ddf6a5c254af10dd901
jdktomcat <jdktomcat@163.com>
2023-03-09 20:51:49 +0800

    代码优化

---
+++ spring-boot-mybatis/src/main/java/com/jdktomcat/spring/boot/mybatis/SpringBootMybatisApplication.java
@@ -0,0 +1,12 @@
+package com.jdktomcat.spring.boot.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisApplication.class, args);
+    }
+}
